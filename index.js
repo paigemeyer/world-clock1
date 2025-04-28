89169bb3198d4d3703e53416dfc710a1a9e25211
paigemeyer <paigemeyer71@icloud.com>
2025-04-28 14:23:43 +0200

    added function to the select options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function realTime(){
 
 let sydneyElement= document.querySelector("#sydney");
+
 let sydneyDateElement= sydneyElement.querySelector(".date");
 let sydneyTimeElement= sydneyElement.querySelector(".time");
 let sydneyTime= moment().tz("Australia/Sydney");
@@ -9,11 +10,32 @@ sydneyTimeElement.innerHTML= sydneyTime.format("h:mm:ss [<small>]A[</small>]");
 
 
 let tokyoElement= document.querySelector("#tokyo");
+
 let tokyoDateElement= tokyoElement.querySelector(".date");
 let tokyoTimeElement= tokyoElement.querySelector(".time");
 let tokyoTime= moment().tz("Asia/Tokyo");
 tokyoDateElement.innerHTML= moment().format("MMMM Do yyyy");
 tokyoTimeElement.innerHTML= tokyoTime.format("h:mm:ss [<small>]A[</small>]");}
 
+
+function updateCity(event){
+   let cityTimeZone= event.target.value;
+   let cityName= cityTimeZone.split("/")[1];
+   let cityTime=moment().tz(cityTimeZone);
+   let citiesElement= document.querySelector("#cities");
+   citiesElement.innerHTML=
+   `<div class="city">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do yyyy")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+</div>`
+}
+
 realTime();
 setInterval(realTime,1000);
+
+
+let citiesSelectElement= document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
